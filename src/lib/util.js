3557dd4a0618f0eb32bc79306d8d6c0d7ff9140c
YuHeng Sun <butsalt@163.com>
2019-06-27 11:20:54 +0800

    enhance touch support check

--- src/lib/util.js
+++ src/lib/util.js
@@ -30,23 +30,10 @@ export const env = {
     typeof document !== 'undefined' &&
     'WebkitAppearance' in document.documentElement.style,
   supportsTouch:
-  (
-    function () {
-      var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-      var mq = function(query) {
-        return window.matchMedia(query).matches;
-      }
-
-      if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-        return true;
-      }
-
-      // include the 'heartz' as a way to have a non matching MQ to help terminate the join
-      // https://git.io/vznFH
-      var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
-      return mq(query);
-    }
-  )(),
+    typeof window !== 'undefined' &&
+    ('ontouchstart' in window ||
+      (window.DocumentTouch && document instanceof window.DocumentTouch)) ||
+      navigator.maxTouchPoints > 0,
   supportsIePointer:
     typeof navigator !== 'undefined' && navigator.msMaxTouchPoints,
   isChrome:
